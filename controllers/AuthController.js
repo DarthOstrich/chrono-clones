bcd43a39f4f5763c49711669c720332e84080f6e
Jared <jarednutt@outlook.com>
2017-11-02 22:59:05 -0700

    trying to fix server again...

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const crypto = require('crypto');
+// const crypto = require('crypto');
 const mongoose = require('mongoose');
 const promisify = require('es6-promisify');
 const User = mongoose.model('User');
