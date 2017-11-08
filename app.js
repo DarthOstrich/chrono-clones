e91d2172952d51c19269995571f09e9f65710512
Jared <jarednutt@outlook.com>
2017-11-07 21:37:44 -0800

    added error handling for a 404 page

--- app.js
+++ app.js
@@ -79,5 +79,20 @@ console.log('running the routes');
 // Use the routes!
 app.use('/', routes);
 
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers.notFound);
+
+// One of our error handlers will see if these errors are just validation errors
+app.use(errorHandlers.flashValidationErrors);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors);
+}
+
+// production error handler
+app.use(errorHandlers.productionErrors);
+
 // done! we export it so we can start the site in start.js
 module.exports = app;
